e3a31a63e9c4a5fc004f992f4d89e935b58217a2
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-18 23:57:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,51 +1,49 @@
 require 'pry'
 
 class Song
-  
   attr_accessor :name, :artist, :genre
-    
-    @@count = 0
-    @@artists = []
-    @@genres = []
-  
+
+  @@count = []
+  @@genres = []
+  @@artists = []
+
   def initialize(song_name, artist, genre)
     @name = song_name
     @artist = artist
     @genre = genre
-    @@count += 1
-    @@artists << artist
+    @@count << song_name
     @@genres << genre
-  
+    @@artists << artist
   end
 
   def self.count
-    @@count  
-  end
-  
-  def self.artists
-    @@artists.uniq
+    @@count.length
   end
 
   def self.genres
-    @@genres.uniq
+    @@genres.uniq!
   end
-  
+
+  def self.artists
+    @@artists.uniq!
+  end
+
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre]
+      if genre_count[genre] #if the genre is already in the hash
         genre_count[genre] += 1
-      else
+      else #if it's a new genre
         genre_count[genre] = 1
       end
     end
     genre_count
   end
-  
-    def self.artist_count
+
+  def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist]
+      if artist_count[artist] #if the artist is already in the hash
         artist_count[artist] += 1
       else
         artist_count[artist] = 1
@@ -53,4 +51,4 @@ class Song
     end
     artist_count
   end
-end 
+end
